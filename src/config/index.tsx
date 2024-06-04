5e0c56311a8303068161983d20f84b778d2f0af7
jessie717 <jessiezly717@gmail.com>
2024-06-04 17:50:07 +0800

    config: 配置修改

---
+++ src/config/index.tsx
@@ -0,0 +1,41 @@
+export const CHAIN_LINKS = [
+	{ id: 1, name: 'Arbitrum Sepolia', src: 'https://smartcontract.imgix.net/icons/arbitrum.svg' },
+	{ id: 2, name: 'Avalanche Fuji', src: 'https://smartcontract.imgix.net/icons/avalanche.svg' },
+	{ id: 3, name: 'Base Sepolia', src: 'https://smartcontract.imgix.net/icons/base.svg' },
+	{ id: 4, name: 'BNB Chain Testnet', src: 'https://smartcontract.imgix.net/icons/bsc.svg' },
+	{
+		id: 5,
+		name: 'Celo Alfajores Testnet',
+		src: 'https://smartcontract.imgix.net/icons/celo.svg'
+	},
+	{ id: 6, name: 'Ethereum Sepolia', src: 'https://smartcontract.imgix.net/icons/ethereum.svg' },
+	{ id: 7, name: 'Fantom Testnet', src: 'https://smartcontract.imgix.net/icons/fantom.svg' },
+	{
+		id: 8,
+		name: 'Gnosis Chiado Testnet',
+		src: 'https://smartcontract.imgix.net/icons/xdai.svg'
+	},
+	{ id: 9, name: 'Kroma Sepolia', src: 'https://smartcontract.imgix.net/icons/kroma.svg' },
+	{ id: 10, name: 'Linea Sepolia', src: 'https://smartcontract.imgix.net/icons/linea.svg' },
+	{ id: 11, name: 'Metis Sepolia', src: 'https://smartcontract.imgix.net/icons/metis.svg' },
+	{
+		id: 12,
+		name: 'Optimism Sepolia',
+		src: 'https://smartcontract.imgix.net/icons/optimism.svg'
+	},
+	{ id: 13, name: 'Polygon Amoy', src: 'https://smartcontract.imgix.net/icons/polygon.svg' },
+	{
+		id: 14,
+		name: 'Polygon Cardona zkEVM',
+		src: 'https://smartcontract.imgix.net/icons/polygon-zkevm.svg'
+	},
+	{
+		id: 15,
+		name: 'Scroll Sepolia Testnet',
+		src: 'https://smartcontract.imgix.net/icons/scroll.svg'
+	},
+	{ id: 16, name: 'WEMIX Testnet', src: 'https://smartcontract.imgix.net/icons/wemix.svg' },
+	{ id: 17, name: 'zkSync Sepolia', src: 'https://smartcontract.imgix.net/icons/zksync.svg' }
+]
+
+export const NEXT_WALLET_TOKEN = 'NEXT_WALLET_TOKEN'
